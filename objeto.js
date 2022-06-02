548ce137caf050d0728920f23552c3127c486389
davi vieira <vdsdavi@gmail.com>
2022-06-02 14:04:50 -0300

    melhora no codigo

--- objeto.js
+++ objeto.js
@@ -1,13 +1,16 @@
 let nome = 'davi';
 let idae = 18;
-let altura = 171
+let altura = 171;
 let sobrenome = 'silva';
+let esporte = 'futebol'
 
 let pessoa = {
     nome: 'davi',
     idade: 18,
     altura: 171,
-    sobrenome = 'silva',
+    sobrenome:'silva',
+    esporte: 'futebol',
+
 };
 
 console.log (pessoa);
