e8a6d8af4baf25f6a7107ba291f4899743cf2443
davi vieira <vdsdavi@gmail.com>
2022-06-02 00:16:13 -0300

    primeiro commit

---
+++ objeto.js
@@ -0,0 +1,13 @@
+let nome = 'davi';
+let idae = 18;
+let altura = 171
+let sobrenome = 'silva';
+
+let pessoa = {
+    nome: 'davi',
+    idade: 18,
+    altura: 171,
+    sobrenome = 'silva',
+};
+
+console.log (pessoa);
