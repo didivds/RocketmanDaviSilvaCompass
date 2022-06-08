99058e4985c8974dd1b9e46cf4d4b4cca3c9156a
davi vieira <vdsdavi@gmail.com>
2022-06-07 23:20:17 -0300

    começando com mocha chai

---
+++ sample.spec.js
@@ -0,0 +1,10 @@
+
+import assert from "assert"
+
+describe('o começo dos testes', () => {
+    it ('verificar uma igualdade', () => {
+        let aux = 101
+        assert.strictEqual(aux, 101)
+    })
+})
+
